09c1232c138fe28c360b1259f2d0a100c003e9d6
jrdhansen <31711241+jrdhansen@users.noreply.github.com>
2017-11-09 17:02:13 -0700

    Add files via upload

---
+++ JaredHansen_AmericanBinomialCYTHON.py
@@ -0,0 +1,88 @@
+
+## Binomial Model for American-style options in Cython
+## Brough named this file deacon.pyx in the server (engine.pyx on gitHub)
+
+
+cimport numpy as np
+
+
+
+cdef class Payoff:
+    """ An abstract base class for option payoffs."""
+    def __init__(self, strike):
+        self._strike = strike
+
+
+cdef class VanillaCallPayoff(Payoff):
+    """A concrete class for plain vanilla call option payoff."""
+    cpdef payoff(self, double spot):
+        return np.maximum(spot - self._strike, 0.0)
+
+
+# we'll also want a class for Put options called
+# cdef class VanillaPutPayoff(Payoff):
+
+
+
+
+cdef class Option:
+    def __init__(self, expiry, payoff):
+        self._expiry = expiry
+        self._payoff = payoff
+    cpdef payoff(self, double spot):
+        return self._payoff.payoff(spot)
+
+
+
+
+cdef class PricingEngine:
+    """ An abstract base class for option pricing engines."""
+    cdef double calculate(self, Option option, MarketData data):
+        pass
+
+
+
+
+cdef class BinomialEngine(PricingEngine):
+    """ An interface class for binomial pricing engines."""
+    def __init__(self, nsteps):
+        self._nsteps = nsteps
+
+    cdef double calculate(self, Option option, MarketData data):
+        pass
+
+
+cdef class AmericanBinomialEngine(BinomialEngine):
+    """ A concrete class for the american binomial option pricing model"""
+    cdef double calculate(self, Option option, MarketData data):
+        cdef double dt = option.expiry / self._nsteps
+        cdef double u = np.exp((data.rate - data.div) * dt + data.vol * np.sqrt(dt))
+        cdef double d = np.exp((data.rate - data.div) * dt - data.vol * np.sqrt(dt))
+        cdef double pu = (np.exp((data.rate - data.div) * dt) - d) / (u-d)
+        cdef double pd = 1.0 - pu
+        cdef double disc = np.exp(-data.rate * dt)
+        cdef double dpu = disc * pu
+        cdef double dpd = disc * pd
+
+        cdef long numNodes = self._nsteps + 1
+        cdef double [:] spot_t = np.empty(numNodes, dtype = np.float64)
+        cdef double [:] call_t = np.empty(numNodes, dtype = np.float64)
+        cdef int i
+
+
+# modify the stuff below this line: it's just coming from the python version
+# I think that Ct = call_t, St = spot_t, nodes = numNodes, steps = self._nsteps
+    # "Ct" = option value at time t, "St" = spot value at time t
+
+
+        for i in range(numNodes):
+            spot_t[i] = spot * (u ** (self._nsteps - i)) * (d ** i)
+            call_t[i] = option.payoff(spot_t[i])
+
+        for i in range((self._nsteps - 1), -1, -1):
+            for j in range(i + 1):
+                call_t[j] = dpu * call_t[j] + dpd * call_t[j + 1]
+                spot_t[j] = spot_t[j] / u
+                call_t[j] = np.maximum(call_t[j], option.payoff(spot_t[j]))
+
+        return call_t[0]
